d5ae55c83dad01f3b69ba38775e6f3c6e5c15624
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2025-07-29 12:46:02 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 840 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,16 @@
+
+def move(start, end):
+    print(start, end)
+
+def hanoi(n, start, end, sub):
+    if n == 1:
+        move(start, end)
+    else:
+        hanoi(n-1, start, sub, end)
+        move(start, end)
+        hanoi(n-1, sub, end, start)
+
+
+n = int(input())
+print((2**n)-1)
+hanoi(n, 1, 3, 2)
